2216b24b2827eefabf01229262d2fa1061980a6f
Sem <40010@ma-web.nl>
2025-12-12 13:06:24 +0100

    first commit

---
+++ classes/RijSchool/RijSchool.cs
@@ -0,0 +1,150 @@
+﻿namespace RijSchool;
+
+class RijSchool
+{
+}
+
+class Auto
+{
+    internal bool automaat;
+    internal string merk;
+    internal string Kenteken;
+    internal int KilometerStand;
+
+}
+
+class Dag
+{
+    internal DateTime datum;
+    internal LesUur[] lesuren;
+}
+
+class LesUur
+{
+    internal int tijd;
+    internal Student student;
+    internal RijLeraar rijLeraar;
+    internal Auto auto;
+}
+
+class LesPakket
+{
+    internal int urenGekocht;
+    internal int urenVerbruikt;
+    internal int examenPogingen;
+    internal bool Automaat;
+
+
+}
+
+class Program
+{
+}
+class RijLeraar
+{
+    internal string naam;
+    internal int leeftijd;
+    internal bool zzp;
+}
+class RijTest
+{
+    internal bool gehaald;
+    internal DateTime afnameDatum;
+}
+class Student
+{
+    internal string naam;
+    internal int leeftijd;
+    internal LesPakket lesPakket;
+    internal RijLeraar rijLeraar;
+    internal TheorieTest theorieTest;
+    internal RijTest rijTest;
+}
+
+class TheorieTest
+{
+    internal int aantalFouten;
+    internal bool gehaald;
+    internal DateTime afnameDatum;
+
+}
+
+// Hier is de run command en de informatie van de rijschool.
+class Informatie
+{
+
+    static void Main(string[] args)
+    {
+        Auto auto1 = new Auto()
+        {
+            merk = "BMW",
+            Kenteken = "163-DOE-93",
+            KilometerStand = 25000,
+            automaat = true
+        };
+
+        Auto auto2 = new Auto()
+        {
+            merk = "Volvo",
+            Kenteken = "123-ABC-45",
+            KilometerStand = 15000,
+            automaat = false
+        };
+
+        RijLeraar Rijleraar = new RijLeraar()
+        {
+            naam = "Joost Marijnissen",
+            leeftijd = 32,
+            zzp = false
+        };
+
+        LesUur lesUur1 = new LesUur()
+        {
+            auto = auto1,
+            rijLeraar = Rijleraar,
+            tijd = 1320
+        };
+        LesUur lesUur2 = new LesUur()
+        {
+            auto = auto2,
+            rijLeraar = Rijleraar,
+            tijd = 1515
+        };
+
+        LesPakket lesPakket = new LesPakket()
+        {
+            urenGekocht = 40,
+            urenVerbruikt = 13,
+            examenPogingen = 1,
+            Automaat = true
+        };
+        TheorieTest theorieTest = new TheorieTest()
+        {
+            aantalFouten = 14,
+            gehaald = false,
+            afnameDatum = new DateTime(2025, 12, 26)
+        };
+
+        RijTest rijTest = new RijTest()
+        {
+            gehaald = false,
+            afnameDatum = new DateTime(2026, 1, 31)
+        };
+
+        Student student = new Student()
+        {
+            naam = "Sem Doeswijk",
+            leeftijd = 18,
+            lesPakket = lesPakket,
+            rijLeraar = Rijleraar,
+            theorieTest = theorieTest,
+            rijTest = rijTest
+        };
+
+        Dag dag = new Dag()
+        {
+            datum = new DateTime(2026, 2, 27),
+            lesuren = new LesUur[] { lesUur1, lesUur2 }
+        };
+    }
+}
---
+++ functions/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace functions;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+        // het variable die we hebben gemaakt is "program"
+        // "Program" de return type is "new"
+
+    }
+
+//hier komt je function
+internal void Run()
+{
+ Console.WriteLine("Dit is nu de start van mijn programma.");
+}
+
+internal void Vraag1()
+{
+ Console.WriteLine("How long do you think you'd survive a zombie apocalypse?");
+ string antwoord = Console.ReadLine();
+
+ Console.WriteLine(antwoord); 
+
+}
+}
