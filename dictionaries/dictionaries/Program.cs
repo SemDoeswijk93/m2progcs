6216bebe845d604db8fd91f457911c75d3617231
Sem <40010@ma-web.nl>
2026-01-09 12:27:20 +0100

    Add initial implementation of dictionaries project with basic functionality

---
+++ dictionaries/dictionaries/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace dictionaries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> Personeelopnummer = new Dictionary<int, string>();
+        Personeelopnummer.Add(9327, "Jan");
+        Personeelopnummer.Add(456, "Jesse");
+
+        string jan = Personeelopnummer[9327];
+        Console.WriteLine(jan);
+    }
+}
