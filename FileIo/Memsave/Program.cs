b0ed4f7506353b6e909e49a11e086d860c126064
Sem Doeswijk <40010@ma-web.nl>
2026-01-23 13:02:13 +0100

    FileIo Opdracht 3

---
+++ FileIo/Memsave/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Memsave;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string newString = "Please Enter";
+
+        while (true)
+        {
+            bool bestaatfile = File.Exists("welkomstate.txt");
+            if (bestaatfile == true)
+            {
+                string[] lines = File.ReadAllText("welkomstate.txt");
+                foreach (string line in lines)
+                {
+                    File.WriteAllText("welkomstate.txt", lines);
+                }
+            }
+            Console.WriteLine("Hello, World!");
+            string[] welkom = File.ReadAllLines("welkomstate.txt");
+            foreach (string line in welkom)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine(newString);
+            Console.ReadLine();
+        }
+    }
+}
